6e22a6c696be11a291c98e7a0e4df95c3b6a8483
Russell LaCour <russelllacour@gmail.com>
2019-11-05 19:46:18 -0800

    Uncomment + add proper tradeapi

--- connect.go
+++ connect.go
@@ -1,64 +1,99 @@
 package algo
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"gitlab.com/raedah/tradeapi"
+	"gitlab.com/raedah/tradeapi/iex"
+)
+
 func Connect(settingsFile string, secret bool, algo Algo, rebalance func(float64, *Algo)) {
-	// config = loadConfiguration(settingsFile, secret)
-	// // settings = loadConfiguration("dev/mm/testnet", true)
-	// log.Println(config)
-	// // fireDB := setupFirebase()
+	config = loadConfiguration(settingsFile, secret)
+	// settings = loadConfiguration("dev/mm/testnet", true)
+	log.Println(config)
+	// fireDB := setupFirebase()
 
-	// exchangeVars := iex.ExchangeConf{
-	// 	Exchange:       "binance",
-	// 	ApiSecret:      config.APISecret,
-	// 	ApiKey:         config.APIKey,
-	// 	AccountID:      "test",
-	// 	OutputResponse: false,
-	// }
+	exchangeVars := iex.ExchangeConf{
+		Exchange:       "binance",
+		ApiSecret:      config.APISecret,
+		ApiKey:         config.APIKey,
+		AccountID:      "test",
+		OutputResponse: false,
+	}
 
-	// base_currency := "USD"
-	// quote_currency := "BTC"
+	base_currency := "USDT"
+	quote_currency := "BTC"
 
-	// ex, err := exchangehandler.GetExchangeType(exchangeVars)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
+	ex, err := tradeapi.New(exchangeVars)
+	if err != nil {
+		fmt.Println(err)
+	}
 
-	// //Get base and quote balances
-	// baseCurrencyBalance, err := ex.GetBalance(base_currency)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// fmt.Printf("base_currency balance: %+v \n", baseCurrencyBalance.Available)
+	//Get base and quote balances
+	baseCurrencyBalance, err := ex.GetBalance(base_currency)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("base_currency balance: %+v \n", baseCurrencyBalance.Available)
 
-	// algo.Asset.BaseBalance = baseCurrencyBalance.Available
+	algo.Asset.BaseBalance = baseCurrencyBalance.Available
 
-	// quoteCurrencyBalance, err := ex.GetBalance(quote_currency)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// fmt.Printf("quote_currency balance: %+v \n", quoteCurrencyBalance.Available)
-	// algo.Asset.Quantity = quoteCurrencyBalance.Available
+	quoteCurrencyBalance, err := ex.GetBalance(quote_currency)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("quote_currency balance: %+v \n", quoteCurrencyBalance.Available)
+	algo.Asset.Quantity = quoteCurrencyBalance.Available
 
-	// mkt, err := ex.GetMarketSummary(quote_currency, base_currency)
-	// // fmt.Printf("markets: %+v \n", mkt)
-	// algo.Asset.Price = mkt.Last
-	// fmt.Printf("algo.Asset.Price: %+v \n", mkt.Last)
-	// rebalance(mkt.Last, &algo)
-	// // algo.BuyOrders.Quantity = mulArr(algo.BuyOrders.Quantity, (algo.Asset.Buying * mkt.Last))
-	// // algo.SellOrders.Quantity = mulArr(algo.SellOrders.Quantity, (algo.Asset.Selling * mkt.Last))
-	// log.Println("algo.Asset.BaseBalance", algo.Asset.BaseBalance)
-	// log.Println("Total Buy BTC", (algo.Asset.Buying))
-	// log.Println("Total Buy USD", (algo.Asset.Buying * mkt.Last))
-	// log.Println("Total Sell BTC", (algo.Asset.Selling))
-	// log.Println("Total Sell USD", (algo.Asset.Selling * mkt.Last))
-	// // log.Println("Local order length", len(orders))
-	// log.Println("New order length", len(algo.BuyOrders.Quantity), len(algo.SellOrders.Quantity))
-	// // log.Println("Buys", algo.BuyOrders.Quantity)
-	// // log.Println("Sells", algo.SellOrders.Quantity)
-	// // log.Println("New order length", len(algo.BuyOrders.Price), len(algo.SellOrders.Price))
-	// orders, err := ex.OpenOrders(iex.OpenOrderF{Market: quote_currency + base_currency})
-	// toCreate, toCancel := algo.PlaceOrdersOnBook(orders)
-	// log.Println(toCreate)
-	// log.Println(toCancel)
-	// algo.logState("")
-	// // updateAlgo(fireDB, "mm")
+	mkt, err := ex.GetMarketSummary(quote_currency, base_currency)
+	// fmt.Printf("markets: %+v \n", mkt)
+	algo.Asset.Price = mkt.Last
+	fmt.Printf("algo.Asset.Price: %+v \n", mkt.Last)
+
+	// channels to subscribe to
+	subscribeInfos := []iex.WSSubscribeInfo{
+		{Name: iex.WS_TRADE_BIN_1_MIN, Symbol: strings.ToLower(quote_currency + base_currency)},
+	}
+
+	// Channels for recieving websocket response.
+	channels := &iex.WSChannels{
+		TradeBinChan: make(chan []iex.TradeBin, 2),
+	}
+
+	// Start the websocket.
+	err = ex.StartWS(&iex.WsConfig{Host: "", Streams: subscribeInfos, Channels: channels})
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	for {
+		select {
+		case update := <-channels.TradeBinChan:
+			fmt.Printf("Trade Update: %+v \n", update)
+		}
+	}
+
+	rebalance(mkt.Last, &algo)
+	// algo.BuyOrders.Quantity = mulArr(algo.BuyOrders.Quantity, (algo.Asset.Buying * mkt.Last))
+	// algo.SellOrders.Quantity = mulArr(algo.SellOrders.Quantity, (algo.Asset.Selling * mkt.Last))
+	log.Println("algo.Asset.BaseBalance", algo.Asset.BaseBalance)
+	log.Println("Total Buy BTC", (algo.Asset.Buying))
+	log.Println("Total Buy USD", (algo.Asset.Buying * mkt.Last))
+	log.Println("Total Sell BTC", (algo.Asset.Selling))
+	log.Println("Total Sell USD", (algo.Asset.Selling * mkt.Last))
+	// log.Println("Local order length", len(orders))
+	log.Println("New order length", len(algo.BuyOrders.Quantity), len(algo.SellOrders.Quantity))
+	// log.Println("Buys", algo.BuyOrders.Quantity)
+	// log.Println("Sells", algo.SellOrders.Quantity)
+	// log.Println("New order length", len(algo.BuyOrders.Price), len(algo.SellOrders.Price))
+	orders, err := ex.OpenOrders(iex.OpenOrderF{Market: quote_currency + base_currency})
+	toCreate, toCancel := algo.PlaceOrdersOnBook(orders)
+	log.Println(toCreate)
+	log.Println(toCancel)
+	algo.logState("")
+	// updateAlgo(fireDB, "mm")
 }
