687c7189314bf7c112aa724201c2a5d40dbd6cee
Daniel Wasserman <wassermandaniel8@gmail.com>
2019-12-03 11:52:05 -0800

    Update optionTheo test

--- options/theoEngine_test.go
+++ options/theoEngine_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/tantralabs/TheAlgoV2/tantradb"
+	models "github.com/tantralabs/TheAlgoV2/models"
 )
 
 func Arange(start, stop, step float64) []float64 {
@@ -55,37 +55,46 @@ func Arange(start, stop, step float64) []float64 {
 // 	fmt.Printf("Processed %v options in %v seconds.\n", numOptions, duration)
 // }
 
-func TestBinomialTree(t *testing.T) {
-	testStart := time.Now().UnixNano()
-	start := 1567756800000
-	end := 1571990400000
-	impliedVolData := tantradb.LoadImpliedVols("XBTUSD", start, end)
-	numOptions := 0
-	times := Arange(float64(start), float64(end), 10*86400.*1000)
-	strikes := Arange(5000., 20000., 1000.)
-	timesToExpiry := Arange(7., 63., 7.)
-	for _, time := range times {
-		for _, strike := range strikes {
-			for _, timeToExpiry := range timesToExpiry {
-				for _, currentPrice := range strikes {
-					for _, optionType := range []string{"call", "put"} {
-						fmt.Printf("Time: %v, strike: %v, timeToExpiry: %v, currentPrice: %v, optionType: %v\n", time, strike, timeToExpiry, currentPrice, optionType)
-						impliedVol := GetNearestVol(impliedVolData, int(time))
-						// fmt.Printf("Got nearest vol: %v\n", impliedVol)
-						o := NewOptionTheo(optionType, currentPrice, strike, int(time), int(time+(timeToExpiry*86400)), 0, impliedVol, -1)
-						o.CalcBinomialTreeTheo(.5, 15)
-						fmt.Printf("Got theo %v for %v option with strike %v, days to expiration %v\n", o.binomialTheo, optionType, strike, o.timeLeft*365)
-						numOptions += 1
-					}
-				}
-			}
-		}
-	}
-
-	// o := NewOptionTheo("call", 8000, 10000, int(end), int(end+(10*86400000)), 0, .75, -1)
-	// o.CalcBinomialTreeTheo(.01, .5, 3600)
-	// o.CalcBinomialTreeTheo(.5, 20)
-
-	duration := float64(time.Now().UnixNano()-testStart) / 1000000000
-	fmt.Printf("Processed %v options in %v seconds.\n", numOptions, duration)
+func TestLiveData(t *testing.T) {
+	currentTime := int(time.Now().Unix()) * 1000
+	fmt.Printf("Current time: %v\n", currentTime)
+	o := models.NewOptionTheo("call", 7340, 9000, currentTime, currentTime+(60*86400000), 0, .69, -1)
+	o.CalcBlackScholesTheo(false)
+	o.CalcBinomialTreeTheo(.5, 15)
+	fmt.Printf("Black scholes theo for %v: %v, binomial tree theo: %v", o.String(), o.Theo, o.BinomialTheo)
 }
+
+// func TestBinomialTree(t *testing.T) {
+// 	testStart := time.Now().UnixNano()
+// 	start := 1567756800000
+// 	end := 1571990400000
+// 	impliedVolData := tantradb.LoadImpliedVols("XBTUSD", start, end)
+// 	numOptions := 0
+// 	times := Arange(float64(start), float64(end), 10*86400.*1000)
+// 	strikes := Arange(5000., 20000., 1000.)
+// 	timesToExpiry := Arange(7., 63., 7.)
+// 	for _, time := range times {
+// 		for _, strike := range strikes {
+// 			for _, timeToExpiry := range timesToExpiry {
+// 				for _, currentPrice := range strikes {
+// 					for _, optionType := range []string{"call", "put"} {
+// 						fmt.Printf("Time: %v, strike: %v, timeToExpiry: %v, currentPrice: %v, optionType: %v\n", time, strike, timeToExpiry, currentPrice, optionType)
+// 						impliedVol := GetNearestVol(impliedVolData, int(time))
+// 						// fmt.Printf("Got nearest vol: %v\n", impliedVol)
+// 						o := NewOptionTheo(optionType, currentPrice, strike, int(time), int(time+(timeToExpiry*86400)), 0, impliedVol, -1)
+// 						o.CalcBinomialTreeTheo(.5, 15)
+// 						fmt.Printf("Got theo %v for %v option with strike %v, days to expiration %v\n", o.binomialTheo, optionType, strike, o.timeLeft*365)
+// 						numOptions += 1
+// 					}
+// 				}
+// 			}
+// 		}
+// 	}
+
+// 	// o := NewOptionTheo("call", 8000, 10000, int(end), int(end+(10*86400000)), 0, .75, -1)
+// 	// o.CalcBinomialTreeTheo(.01, .5, 3600)
+// 	// o.CalcBinomialTreeTheo(.5, 20)
+
+// 	duration := float64(time.Now().UnixNano()-testStart) / 1000000000
+// 	fmt.Printf("Processed %v options in %v seconds.\n", numOptions, duration)
+// }
