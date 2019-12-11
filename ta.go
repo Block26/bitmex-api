50c742115929f644884ba564595b6f2cfcf14051
Josh Horosny <josh@block26.com>
2019-12-11 14:24:27 -0800

    Added TA Calculations
    -Get unstable ta calculations and ema calculation now in ta.go.

---
+++ ta.go
@@ -0,0 +1,37 @@
+package algo
+
+
+import (
+	ta "github.com/d4l3k/talib"
+)
+
+
+func GetNATR(high []float64, low []float64, close []float64, index int32, length int32) []float64 {
+	//TODO check live
+	return ta.Natr(high[index-length-1:index], low[index-length-1:index], close[index-length-1:index], length)
+}
+
+func CreateNATREMA(high []float64, low []float64, close []float64, index int32, natrLength int32, emaLength int32) []float64 {
+	//TODO check live
+	natr := ta.Natr(high[index-natrLength-emaLength-1:index], low[index-natrLength-emaLength-1:index], close[index-natrLength-emaLength-1:index], natrLength)
+	ema := CreateEMA(natr, emaLength)
+	return ema
+}
+
+func GetDX(high []float64, low []float64, close []float64, index int32, length int32) []float64 {
+	//TODO check live
+	return ta.Dx(high[index-length-1:index], low[index-length-1:index], close[index-length-1:index], length)
+}
+
+func CreateEMA(data []float64, length int32) []float64 {
+	newEma := make([]float64, len(data))
+	ema := ta.Ema(data, length)
+	for x := range newEma {
+		if x < int(length) {
+			newEma[x] = 0
+		} else {
+			newEma[x] = ema[x-int(length)]
+		}
+	}
+	return newEma
+}
