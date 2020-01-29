a43957486ddfc9c597d74f0dab6266dd86e2cec4
Russell LaCour <russelllacour@gmail.com>
2020-01-28 17:00:00 -0800

    Added Config Model

--- models/config.go
+++ models/config.go
@@ -1,6 +1,19 @@
 package models
 
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
 type Config struct {
-	APIKey    string `json:"api_key"`
-	APISecret string `json:"api_secret"`
+	Name     string `json: "name"`
+	Exchange string `json: "exchange"`
+	Symbol   string `json: "symbol"`
+	Secret   string `json: "secret"`
+}
+
+func Load(fileName string) (config Config) {
+	file, _ := ioutil.ReadFile(fileName)
+	_ = json.Unmarshal([]byte(file), &config)
+	return
 }
