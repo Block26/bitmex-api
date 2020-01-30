00b8b7a72282edf683e9c9c1dad61676804b891c
Russell LaCour <russelllacour@gmail.com>
2020-01-29 16:33:06 -0800

    Changed from Load to LoadConfig

--- models/config.go
+++ models/config.go
@@ -6,13 +6,15 @@ import (
 )
 
 type Config struct {
-	Name     string `json:"name"`
-	Exchange string `json:"exchange"`
-	Symbol   string `json:"symbol"`
-	Secret   string `json:"secret"`
+	Algo       string `json:"algo"`
+	Name       string `json:"name"`
+	Exchange   string `json:"exchange"`
+	Symbol     string `json:"symbol"`
+	Secret     string `json:"secret"`
+	CommitHash string `json:"commit_hash"`
 }
 
-func Load(fileName string) (config Config) {
+func LoadConfig(fileName string) (config Config) {
 	file, _ := ioutil.ReadFile(fileName)
 	_ = json.Unmarshal([]byte(file), &config)
 	return
