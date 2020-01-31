21522b683ef87e23e9417c85f3485170ccf9309e
Daniel Wasserman <wassermandaniel8@gmail.com>
2020-01-31 13:12:27 -0800

    Add utils.StringInSlice

--- utils/utils.go
+++ utils/utils.go
@@ -396,3 +396,12 @@ func IntInSlice(a int, list []int) bool {
 	}
 	return false
 }
+
+func StringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
