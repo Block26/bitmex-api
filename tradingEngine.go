a11a400b46d4b7479d649ff6c218cef4018d5b10
Daniel Wasserman <wassermandaniel8@gmail.com>
2020-04-14 11:19:20 -0700

    Update stopping conditions

--- tradingEngine.go
+++ tradingEngine.go
@@ -312,7 +312,7 @@ func (t *TradingEngine) Connect(settingsFileName string, secret bool, rebalance 
 			t.aggregateAccountProfit()
 			logger.Infof("[Trading Engine] trade processing took %v ns\n", time.Now().UnixNano()-startTimestamp)
 			channels.TradeBinChanComplete <- nil
-			if !t.Algo.Timestamp.Before(t.endTime) {
+			if t.Algo.Timestamp.After(t.endTime) {
 				logger.Infof("Algo timestamp %v past end time %v, killing trading engine.\n", t.Algo.Timestamp, t.endTime)
 				if isTest {
 					logStats(t.Algo, history, startTime)
